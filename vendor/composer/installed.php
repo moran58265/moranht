343a004b8696617d0869b6866376a5296149aa6e
hu58265king <2659917175@qq.com>
2022-05-18 10:23:30 +0800

    new

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '9109ed81dea7a255257c48f9c94316f5bc02104c',
+        'reference' => '8ab1ece724d60ce55103bb6056da50b153553ca7',
         'name' => 'topthink/think',
         'dev' => true,
     ),
@@ -34,7 +34,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '9109ed81dea7a255257c48f9c94316f5bc02104c',
+            'reference' => '8ab1ece724d60ce55103bb6056da50b153553ca7',
             'dev_requirement' => false,
         ),
         'topthink/think-captcha' => array(

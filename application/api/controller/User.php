e7d6d6d8ae2996aba5c3f4e3bde69b07f86c3f4d
hu58265king <2659917175@qq.com>
2022-06-01 09:20:13 +0800

    new

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -448,6 +448,10 @@ class User extends Controller
                     $result['invitecode'] = $userdata['invitecode'];
                     $result['invitetotal'] = $userdata['invitetotal'];
                     $result['inviter'] = $userdata['inviter'];
+                    //评论数
+                    $result['commentnum'] = Db::name('comment')->where('username', $data['username'])->count();
+                    //帖子数量
+                    $result['postnum'] = Db::name('post')->where('username', $data['username'])->count();
                     return Common::return_msg(200, "查询成功", $result);
                 } else {
                     return Common::ReturnError("token错误");
@@ -500,7 +504,7 @@ class User extends Controller
                 $emailresult = Db::name('email')->where('id', 1)->find();
                 $emailcontent = "<h3>您的随机密码是：". $newpassword."<br>请及时修改为您易记的密码</h3>";
                 $emailcontenthtml = '<div><includetail><div align="center"><div class="open_email"style="margin-left: 8px; margin-top: 8px; margin-bottom: 8px; margin-right: 8px;"><div><br><span class="genEmailContent"><div id="cTMail-Wrap"style="word-break: break-all;box-sizing:border-box;text-align:center;min-width:320px; max-width:660px; border:1px solid #f6f6f6; background-color:#f7f8fa; margin:auto; padding:20px 0 30px;"><div class="main-content"style=""><table style="width:100%;font-weight:300;margin-bottom:10px;border-collapse:collapse"><tbody><tr style="font-weight:300"><td style="width:3%;max-width:30px;"></td><td style="max-width:600px;"><h1>'.$emailresult["email_title"].'</h1><p style="height:2px;background-color: #00a4ff;border: 0;font-size:0;padding:0;width:100%;margin-top:20px;"></p><div id="cTMail-inner"style="background-color:#fff; padding:23px 0 20px;box-shadow: 0px 1px 1px 0px rgba(122, 55, 55, 0.2);text-align:left;"><table style="width:100%;font-weight:300;margin-bottom:10px;border-collapse:collapse;text-align:left;"><tbody><tr style="font-weight:300"><td style="width:3.2%;max-width:30px;"></td><td style="max-width:480px;text-align:left;"><h1 id="cTMail-title"style="font-size: 20px; line-height: 36px; margin: 0px 0px 22px;">找回密码</h1><p id="cTMail-userName"style="font-size:14px;color:#333; line-height:24px; margin:0;">尊敬的'.$data["username"].'用户，您好！</p><p class="cTMail-content"style="line-height: 24px; margin: 6px 0px 0px; overflow-wrap: break-word; word-break: break-all;"><span style="color: rgb(51, 51, 51); font-size: 14px;">'.$emailcontent.'</span></p><p class="cTMail-content"style="line-height: 24px; margin: 6px 0px 0px; overflow-wrap: break-word; word-break: break-all;"><span style="color: rgb(51, 51, 51); font-size: 14px;"><span style="font-weight: bold;">请注意密码的大小写。</span></span></p><dl style="font-size: 14px; color: rgb(51, 51, 51); line-height: 18px;"><dd style="margin: 0px 0px 6px; padding: 0px; font-size: 12px; line-height: 22px;"><p id="cTMail-sender"style="font-size: 14px; line-height: 26px; word-wrap: break-word; word-break: break-all; margin-top: 32px;">此致<br><strong>'.$emailresult["email_title"].'</strong></p></dd></dl></td><td style="width:3.2%;max-width:30px;"></td></tr></tbody></table></div></td><td style="width:3%;max-width:30px;"></td></tr></tbody></table></div></div></span><br></div></div></div></includetail></div>';
-                Common::send_mail($user['useremail'], "注册验证码", $emailcontenthtml);
+                Common::send_mail($user['useremail'], "重置密码", $emailcontenthtml);
                 //Common::send_mail($user['useremail'], "随机密码", "您的随机密码是：" . $newpassword);
                 Db::name('passcode')->where('id', 1)->update(["passcode" => "", "ip" => "", "creattime" => ""]);
             } catch (DataNotFoundException $e) {
@@ -805,6 +809,9 @@ class User extends Controller
         }
         if ($user['user_token'] != $data['usertoken']) {
             return Common::ReturnError("token过期");
+        }
+        if($user['password'] == md5($data['newpwd'])){
+            return Common::ReturnError("修改成功");
         }
         $result = Db::name('user')->where('username', $data['username'])->where('appid', $data['appid'])->update(['password' => md5($data['newpwd'])]);
         if ($result > 0) {

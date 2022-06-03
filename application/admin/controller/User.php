775746439057bc976389686a427b621f134a8393
morannb <2659917175@qq.com>
2022-06-04 00:09:25 +0800

    新增一键生成邀请码

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -17,25 +17,25 @@ class User extends BaseController
     {
         return $this->fetch();
     }
-//获取所有信息
+    //获取所有信息
     public function GetUserlist()
     {
         $nowdaytime = strtotime(date('Y-m-d'));
-        $limit = input('limit')?input('limit'):10;
-        $page = input('page')?input('page'):1;
-        $sort = input('sort')?input('sort'):'id';
-        $sortOrder = input('sortOrder')?input('sortOrder'):'desc';
-        $username = input('username')?input('username'):'';
-        $ipaddress = input('ipaddress') == 0?2:input('ipaddress');
-        $fvip = input('fvip') == 'true'?time():1;
-        $fsign = input('fsign') == 'true'?$nowdaytime:0;
+        $limit = input('limit') ? input('limit') : 10;
+        $page = input('page') ? input('page') : 1;
+        $sort = input('sort') ? input('sort') : 'id';
+        $sortOrder = input('sortOrder') ? input('sortOrder') : 'desc';
+        $username = input('username') ? input('username') : '';
+        $ipaddress = input('ipaddress') == 0 ? 2 : input('ipaddress');
+        $fvip = input('fvip') == 'true' ? time() : 1;
+        $fsign = input('fsign') == 'true' ? $nowdaytime : 0;
         try {
             $appList = Db::name('user')->alias('u')
                 ->join('app a', 'a.appid=u.appid')
                 ->field('u.id,u.username,u.usertx,u.nickname,a.appname,u.useremail,FROM_UNIXTIME(u.creattime,"%Y-%m-%d %H:%i:%s") as creattime,u.banned,ip')
                 ->where('u.username', "like", '%' . $username . '%')
-                ->where('u.viptime', ">=",$fvip)
-                ->where('u.signtime', ">=",$fsign)
+                ->where('u.viptime', ">=", $fvip)
+                ->where('u.signtime', ">=", $fsign)
                 ->order($sort, $sortOrder)
                 ->limit($limit)
                 ->page($page)
@@ -50,28 +50,28 @@ class User extends BaseController
                 $datauserinfo[$key]['useremail'] = $value['useremail'];
                 $datauserinfo[$key]['creattime'] = $value['creattime'];
                 $datauserinfo[$key]['banned'] = $value['banned'];
-                if($ipaddress == 2){
+                if ($ipaddress == 2) {
                     $datauserinfo[$key]['ip'] = $value['ip'];
-                }else{
-                    $datauserinfo[$key]['ip'] = $value['ip']."(".Common::get_ip_address($value['ip']).")";
-                }   
+                } else {
+                    $datauserinfo[$key]['ip'] = $value['ip'] . "(" . Common::get_ip_address($value['ip']) . ")";
+                }
             }
             $appcount = Db::name('user')
                 ->alias('u')
                 ->join('app a', 'a.appid=u.appid')
                 ->field('u.id,u.username,u.usertx,u.nickname,a.appname,u.useremail,FROM_UNIXTIME(u.creattime,"%Y-%m-%d %H:%i:%s") as creattime,u.banned')
                 ->where('u.username', "like", '%' . $username . '%')
-                ->where('u.viptime', ">=",$fvip)
-                ->where('u.signtime', ">=",$fsign)
+                ->where('u.viptime', ">=", $fvip)
+                ->where('u.signtime', ">=", $fsign)
                 ->count();
         } catch (DataNotFoundException $e) {
             return Common::ReturnError($e->getMessage());
-        }catch (ModelNotFoundException $e) {
+        } catch (ModelNotFoundException $e) {
             return Common::ReturnError($e->getMessage());
-        }catch (DbException $e) {
+        } catch (DbException $e) {
             return Common::ReturnError($e->getMessage());
         }
-        return json(['rows' => $datauserinfo,'total' => $appcount]);
+        return json(['rows' => $datauserinfo, 'total' => $appcount]);
     }
 
 
@@ -86,9 +86,8 @@ class User extends BaseController
     //修改用户禁用状态
     public function editfastatus()
     {
-        $id = explode(",",input('id'));
-        for ($i=0;$i<count($id);$i++)
-        {
+        $id = explode(",", input('id'));
+        for ($i = 0; $i < count($id); $i++) {
             $app = UserModel::get($id[$i]);
             $app->banned = 'true';
             $app->save();
@@ -99,9 +98,8 @@ class User extends BaseController
     //修改用户正常状态
     public function edittrstatus()
     {
-        $id = explode(",",input('id'));
-        for ($i=0;$i<count($id);$i++)
-        {
+        $id = explode(",", input('id'));
+        for ($i = 0; $i < count($id); $i++) {
             $app = UserModel::get($id[$i]);
             $app->banned = 'false';
             $app->save();
@@ -114,17 +112,17 @@ class User extends BaseController
     {
         $id = input('id');
         $user = UserModel::get($id);
-        return $this->fetch()->assign('user',$user);
+        return $this->fetch()->assign('user', $user);
     }
     //修改user信息
     public function edituser()
     {
         $data = input('post.');
         $user = new UserModel();
-        $userpassword = $user->where('id',$data['id'])->value('password');
-        if(input('post.password') == "" || input('post.password') == null){
+        $userpassword = $user->where('id', $data['id'])->value('password');
+        if (input('post.password') == "" || input('post.password') == null) {
             $data['password'] = $userpassword;
-        }else{
+        } else {
             $data['password'] = md5(input('post.password'));
         }
         $data['viptime'] = strtotime($data['viptime']);
@@ -141,11 +139,11 @@ class User extends BaseController
     {
         $data = input('post.');
         $validate = new UserValidate();
-        if (!$validate->scene('adduser')->check($data)){
+        if (!$validate->scene('adduser')->check($data)) {
             return Common::ReturnError($validate->getError());
         }
         $useremail = UserModel::get(['useremail' => $data['useremail']]);
-        $username = Db::name('user')->where('username',$data['username'])->where('appid',$data['appid'])->find();
+        $username = Db::name('user')->where('username', $data['username'])->where('appid', $data['appid'])->find();
         $app = \app\admin\model\App::get($data['appid']);
         if ($app == null) {
             return Common::ReturnError('应用不存在');
@@ -159,7 +157,7 @@ class User extends BaseController
         //截取@qq.com前面的字符串
         $user = new UserModel();
         $data['password'] = md5($data['password']);
-        $data['viptime'] = time()+$app->zcvip*24*60*60;
+        $data['viptime'] = time() + $app->zcvip * 24 * 60 * 60;
         $data['money'] = $app->zcmoney;
         $data['exp'] = $app->zcexp;
         $data['usertx'] =  Request::scheme() . "://" . Request::host() . "/" . "usertx.png";
@@ -169,4 +167,21 @@ class User extends BaseController
         return Common::ReturnSuccess('添加成功');
     }
 
+    public function getallinvitecode()
+    {
+        $user = UserModel::where('invitecode is NULL')->all();
+        if (count($user) == 0) {
+            return Common::ReturnError('没有用户可以生成邀请码');
+        } else {
+            foreach ($user as $key => $value) {
+                $id = $value->id;
+                $username = $value->username;
+                $invitecode = Common::getinvitacode($username);
+                $updateuser = UserModel::get($id);
+                $updateuser->invitecode = $invitecode;
+                $updateuser->save();
+            }
+            return Common::ReturnSuccess('所有用户邀请码已生成');
+        }
+    }
 }
